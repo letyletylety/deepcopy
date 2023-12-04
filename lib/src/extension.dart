c43205df094383a003a3ecfccf569dddfec0fef8
Sun Jiao <P.ozzy@outlook.com>
2023-12-04 15:20:06 +0800

    add generic support

--- lib/src/extension.dart
+++ lib/src/extension.dart
@@ -1,22 +1,22 @@
 import 'package:deepcopy/src/cloner.dart';
 
 /// list extension for deepcopy
-extension ListCloner on List {
-  List deepcopy() {
+extension ListCloner<T> on List<T> {
+  List<T> deepcopy() {
     return NormalCloner().deepcopyList(this);
   }
 }
 
 /// set extension for deepcopy
-extension SetCloner on Set {
-  Set deepcopy() {
+extension SetCloner<T> on Set<T> {
+  Set<T> deepcopy() {
     return NormalCloner().deepcopySet(this);
   }
 }
 
 /// map extension for deepcopy
-extension MapClone on Map {
-  Map deepcopy() {
+extension MapClone<K, V> on Map<K, V> {
+  Map<K, V> deepcopy() {
     return NormalCloner().deepcopyMap(this);
   }
 }
