f24907a89884021a413d022a48b8e0c2aa25d310
Lety <taej0127@gmail.com>
2023-12-05 17:45:05 +0900

    switch to doc comment

--- lib/src/subtype.dart
+++ lib/src/subtype.dart
@@ -1,4 +1,5 @@
-// subtype checker
+/// subtype checker
+///
 // from lrn's StackOverflow answer: https://stackoverflow.com/a/70670060/13447926
 // CC BY-SA 4.0
 bool isSubtype<S, T>() => <S>[] is List<T>;
